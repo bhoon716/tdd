ad0bc718c7853ef6f0b6caffcb1af78204d50a48
박병훈 <leafloveu@naver.com>
2025-11-19 21:12:43 +0900

    feat(gradle): mockk 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("io.mockk:mockk:1.13.13")
 }
 
 kotlin {
