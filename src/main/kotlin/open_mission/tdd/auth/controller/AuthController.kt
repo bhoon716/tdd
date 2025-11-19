41d3dd880eb02de494f556252bcab74b472b9f96
박병훈 <leafloveu@naver.com>
2025-11-19 22:52:32 +0900

    feat: 회원가입 기능 추가

---
+++ src/main/kotlin/open_mission/tdd/auth/controller/AuthController.kt
@@ -0,0 +1,26 @@
+package open_mission.tdd.auth.controller
+
+import open_mission.tdd.auth.request.SignupRequest
+import open_mission.tdd.auth.response.SignupResponse
+import open_mission.tdd.auth.service.AuthService
+import open_mission.tdd.common.response.ApiResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+
+@RestController
+@RequestMapping("/api/auth")
+class AuthController(private val authService: AuthService) {
+
+    @PostMapping("/signup")
+    fun signup(
+        @RequestBody request: SignupRequest
+    ): ResponseEntity<ApiResponse<SignupResponse>> {
+        val response = authService.signup(request)
+        val location = URI.create("/api/users/${response.id}")
+        return ResponseEntity.created(location).body(ApiResponse.success(response, "회원가입 성공"))
+    }
+}
