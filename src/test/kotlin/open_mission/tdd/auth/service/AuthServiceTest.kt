327f929878f5c52d5dbd2d7495293ebccd152522
박병훈 <leafloveu@naver.com>
2025-11-19 22:01:37 +0900

    test(auth): 회원가입 실패 - 중복 이메일 테스트 추가

--- src/test/kotlin/open_mission/tdd/auth/service/AuthServiceTest.kt
+++ src/test/kotlin/open_mission/tdd/auth/service/AuthServiceTest.kt
@@ -5,7 +5,10 @@ import io.mockk.mockk
 import open_mission.tdd.auth.entity.User
 import open_mission.tdd.auth.repository.UserRepository
 import open_mission.tdd.auth.request.SignupRequest
+import open_mission.tdd.common.error.CustomException
+import open_mission.tdd.common.error.ErrorCode
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -23,7 +26,7 @@ class AuthServiceTest {
         // given
         val request = SignupRequest("email@test.com", "password123")
 
-        every { userRepository.findByEmail("email@test.com") } returns null
+        every { userRepository.existsByEmail("email@test.com") } returns false
         every { passwordEncoder.encode("password123") } returns "encodedPassword"
         every { userRepository.save(any()) } returns User(1L, "email@test.com", "encodedPassword")
 
@@ -33,5 +36,21 @@ class AuthServiceTest {
         // then
         assertThat(response.id).isEqualTo(1L)
         assertThat(response.email).isEqualTo("email@test.com")
+    }
+
+    @DisplayName("회원가입 실패 테스트 - 중복 이메일")
+    @Test
+    fun duplicatedEmailTest() {
+        // given
+        val request = SignupRequest("email@test.com", "password123")
+
+        every { userRepository.existsByEmail("email@test.com") } returns true
+//        every { passwordEncoder.encode("password123") } returns "encodedPassword"
+//        every { userRepository.save(any()) } returns User(1L, "email@test.com", "encodedPassword")
+
+        // when & then
+        assertThatThrownBy{ authService.signup(request)}
+            .isExactlyInstanceOf(CustomException::class.java)
+            .hasMessage(ErrorCode.DUPLICATED_USER_EMAIL.message)
     }
 }
